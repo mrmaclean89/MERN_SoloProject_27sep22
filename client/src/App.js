beb758c19b141785e8bf850e867aadc292fc9744
Michael MacLean <mrmaclean89@gmail.com>
2022-10-07 04:03:22 -0700

    Added job to database via postman, rendered all jobs through JobList component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
+import Login from './components/Login'
+import JobList from './components/JobList'
+import NewJob from './components/NewJob'
+import ViewJob from './components/ViewJob'
+import EditJob from './components/EditJob'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/joblist" element={<JobList/>}/>
+          <Route path="/newjob" element={<NewJob/>}/>
+          <Route path="/viewjob/:id" element={<ViewJob/>}/>
+          <Route path="/editjob/:id" element={<EditJob/>}/>
+        </Routes>
+      </BrowserRouter>
+
+
+      <footer className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +34,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
--- client/src/components/JobList.js
+++ client/src/components/JobList.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import {React,useEffect,useState} from 'react'
+import {useNavigate,Link,useParams} from 'react-router-dom'
+import axios from 'axios'
 
 const JobList = () => {
+
+  const [jobs,setJobs] = useState([])
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/api/jobs`)
+    .then((res) => {
+      console.log(res.data)
+      let list = [...res.data.allJobs]
+      setJobs(list)
+    }).catch((err) => {
+      console.log(err)
+    });
+  }, [])
+  
+
   return (
-    <div>JobList</div>
+    <div>
+      <h1>My Yard, My Jobs.</h1>
+      <div>
+        {jobs.map((job,idx)=>(
+          <button type="" key={idx} className="job-btn">{job.name}</button>
+        ))}
+      </div>
+    </div>
   )
 }
 
